dee53f7e73ce1fb044eecf2594e2b2db018cf85d
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2017-06-06 16:33:51 -0700

    Bad definitions.

--- lib/src/code_push.dart
+++ lib/src/code_push.dart
@@ -4,7 +4,7 @@ library code_push;
 import 'package:js/js.dart';
 import 'dart:async';
 
-@JS('CodePush')
+@JS('codePush')
 class CodePushApi {
 	external static void notifyApplicationReady(Function success, Function error);
 	external static void restartApplication();
@@ -12,7 +12,7 @@ class CodePushApi {
 	external static void getCurrentPackage(Function success, Function error);
 	external static void getPendingPackage(Function success, Function error);
 	external static void checkForUpdate(Function success, Function error, [String deploymentKey = null]);
-	external static void sync([Function callback, options, Function progress]);
+	external static void sync(Function callback, options, Function progress);
 	external static void syncInternal([Function callback, options, Function progress]);
 	external static dynamic getDefaultSyncOptions();
 	external static dynamic getDefaultUpdateDialogOptions();
@@ -21,12 +21,29 @@ class CodePushApi {
 class CodePush {
 	static Future getCurrentPackage() {
 		Completer c = new Completer();
-		CodePushApi.getCurrentPackage(allowInterop((result) {
+		CodePushApi.getCurrentPackage(allowInterop((result, [extra = null]) {
+			if (extra != null) {
+				print(extra);
+			}
 			c.complete(result);
 		}), allowInterop((error) {
 			c.completeError(error);
 		}));
 
+		return c.future;
+	}
+
+	static Future sync([Map<dynamic, dynamic> options = null]) {
+		Completer c = new Completer();
+		CodePushApi.sync(allowInterop((result) {
+			print('got a result from sync');
+			print(result);
+			if (!c.isCompleted) {
+				c.complete(result);
+			}
+		}), {}, allowInterop((progress) {
+
+		}));
 		return c.future;
 	}
 }
