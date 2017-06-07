04f6980f9e39d3e8944235f94ba15ff81db6f486
Nathan Kerr <nathankerr@gmail.com>
2017-06-06 21:33:22 -0700

    More work on code push.

--- lib/src/code_push.dart
+++ lib/src/code_push.dart
@@ -4,7 +4,7 @@ library code_push;
 import 'package:js/js.dart';
 import 'dart:async';
 
-@JS('CodePush')
+@JS('codePush')
 class CodePushApi {
 	external static void notifyApplicationReady(Function success, Function error);
 	external static void restartApplication();
@@ -27,6 +27,25 @@ class CodePush {
 			c.completeError(error);
 		}));
 
+		return c.future;
+	}
+
+	static Future sync() {
+		Completer c = new Completer();
+		CodePushApi.sync(allowInterop((result) {
+			print('result from sync');
+			print(result);
+			if (!c.isCompleted) {
+				c.complete(result);
+			}
+		}), {}, allowInterop((error) {
+			print('error from sync');
+			print(error);
+			if (!c.isCompleted) {
+				c.completeError(error);
+			}
+
+		}));
 		return c.future;
 	}
 }
